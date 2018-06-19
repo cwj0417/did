d204dabf4b9a6b3d9c242b6923203f5c0be48e9f
chenwj <731028571@qq.com>
2018-06-19 14:14:06 +0800

    init

---
+++ src/popup/app.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        额呵呵
+    </div>
+</template>
+<script type="text/ecmascript-6">
+    export default {
+
+    }
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require("path"),
+    webpack = require("webpack"),
+    srcPath = path.join(__dirname, "src");
+
+const entry = {
+    popup: ["babel-polyfill", path.join(srcPath, "scripts", "popup.js")],
+    options: ["babel-polyfill", path.join(srcPath, "scripts", "options.js")],
+    background: ["babel-polyfill", path.join(srcPath, "scripts", "background.js")],
+    match: ["babel-polyfill", path.join(srcPath, "scripts", "match.js")],
+};
+const output = {
+    path: path.resolve("extension", "scripts"),
+    filename: "[name].js"
+};
+const resolve = {
+    extensions: [".js", ".vue"],
+    alias: {
+        "vue$": "vue/dist/vue.esm.js"
+    },
+    modules: [path.join(__dirname, "node_modules"), srcPath]
+};
+const rules = [
+    {
+        test: /\.(eot|woff|woff2|ttf|svg)$/,
+        use: [{
+            loader: "file",
+            options: {
+                name: "../fonts/[name].[ext]"
+            }
+        }]
+    }, {
+        test: /\.vue$/,
+        use: ["vue"]
+    }, {
+        test: /html$/,
+        exclude: /index.html/,
+        use: ["file?name=/[name].html"]
+    }, {
+        test: /\.(png|jpg)$/,
+        use: ["file?name=img/[name].[ext]"]
+    }, {
+        test: /\.css$/,
+        use: ["style", "css"]
+    }, {
+        test: /\.scss$/,
+        use: ["style", "css", "sass"]
+    }, {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: ["babel", "eslint"]
+    }, {
+        test: [/fontawesome-webfont\.svg/, /fontawesome-webfont\.eot/, /fontawesome-webfont\.ttf/, /fontawesome-webfont\.woff/, /fontawesome-webfont\.woff2/],
+        use: ["file?name=fonts/[name].[ext]"]
+    }
+];
+let config = {
+    entry,
+    output,
+    resolve,
+    module: {
+        rules
+    },
+    resolveLoader: {
+        moduleExtensions: ["-loader"]
+    }
+};
+
+module.exports = config;
